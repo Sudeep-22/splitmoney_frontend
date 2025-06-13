c4e3ca873fbe9ed5a808729bbfe1e43b7712b0f8
SudeepPoojari <114012832+Sudeep-22@users.noreply.github.com>
2025-06-13 22:52:37 +0530

    Add Material UI and basic format

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Login from './pages/Login'
+
+function App() {
+
+  return (
+    <>
+      <h1>Yoo this is my app start!!</h1>
+      <Login/>
+    </>
+  )
+}
+
+export default App
